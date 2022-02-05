21dcae4924194c6dd6a72e2349ad0b3f75419f3e
jnlar <jon357@pm.me>
2022-02-05 23:03:13 +1100

    prepend the last input above the input node

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
 const textConsole = document.getElementById('console');
+const inputContainer = document.getElementById('input-container');
 const output = document.getElementById('output')
 
-// take the input, and then try compute it as JS
+const inputs = [];
+let isFirstPrepended = true;
+
 textConsole.addEventListener('keydown', (e) => {
   if (e.key === "Enter") {
     executeInput(e.target.value);
+    return prependPastInput();
   }
 })
 
 function executeInput(input) {
   textConsole.value = '';
+  inputs.push(input);
 
   try {
-    // we need to be able to do 'var, let, const' declarations
+    // TODO: capability to do 'var, let, const' declarations
     let fn = new Function('return ' + input)();
     return output.innerHTML = fn;
   } catch(e) {
     output.innerHTML = e;
+
     throw new Error(e);
+  }
+}
+
+function prependPastInput() {
+  let prepended = document.createElement('div');
+  let text = document.createTextNode(inputs[inputs.length - 1]);
+  let parentNodesLength = inputContainer.childNodes.length
+
+  prepended.appendChild(text);
+
+  if (isFirstPrepended) {
+    inputContainer.insertBefore(prepended, inputContainer.childNodes[0]);
+    return isFirstPrepended = false;
+  } else { 
+    inputContainer.insertBefore(prepended, inputContainer.childNodes[
+      parentNodesLength - 2
+    ]);
   }
 }
