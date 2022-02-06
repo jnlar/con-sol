dddf598430178ff3eae89dfd4d2c37b1c99be1e8
jnlar <jon357@pm.me>
2022-02-06 21:00:18 +1100

    implement clear console shortcuts, more styling

--- src/index.js
+++ src/index.js
@@ -4,60 +4,91 @@ const output = document.getElementById('output');
 
 let state;
 
-// TODO: implement shift + enter newline
+// TODO: 
+// - implement shift + enter newline
 function bindKeyEvents() {
   textConsole.addEventListener('keydown', (e) => {
-    if (e.key === "Enter") {
+    if (e.key == 'l' && e.ctrlKey) {
+      e.preventDefault();
+      return cursorToTop();
+    }
+
+    if (e.key === 'Enter' && e.target.value !== '') {
       executeInput(e.target.value);
+
+      if (state.clearingPastInputs) {
+        return state.clearingPastInputs = false;
+      }
+
       return prependPastInput();
     }
   })
+}
 
-  textConsole.addEventListener('keyup', () => {
-    Object.keys(state.keys).map(key => state.keys[key] = false);
-  })
+function cursorToTop() {
+  document.querySelectorAll('.past-input').forEach(el => {
+    el.remove();
+  });
+}
+
+function outputWasError() {
+  return output.classList.contains('text-red-500');
 }
 
 function executeInput(input) {
   textConsole.value = '';
   state.inputs.push(input);
 
+  if (outputWasError()) {
+    output.classList.remove('text-red-500');
+  }
+
+  if (input === 'clear') {
+    state.clearingPastInputs = true;
+    return cursorToTop();
+  }
+
   try {
-    // TODO: capability to do 'var, let, const' declarations
+    // TODO: implement capability to do 'var, let, const' declarations,
     let fn = new Function('return ' + input)();
     return output.innerHTML = fn;
   } catch(e) {
+    output.classList.add('text-red-500');
     output.innerHTML = e;
-
     throw new Error(e);
   }
 }
 
+function createNewChild() {
+  let newChild = document.createElement('div');
+  let lastInput = document.createTextNode(state.inputs[state.inputs.length - 1]);
+
+  newChild.appendChild(lastInput);
+  newChild.classList.add('past-input');
+
+  return newChild;
+}
+
 function prependPastInput() {
-  let prepended = document.createElement('div');
-  let text = document.createTextNode(state.inputs[state.inputs.length - 1]);
+  let toPrepend = createNewChild();
   let childNodesLength = inputContainer.childNodes.length;
 
-  prepended.appendChild(text);
-
   if (state.isFirstPrepended) {
-    inputContainer.insertBefore(prepended, inputContainer.childNodes[0]);
+    inputContainer.insertBefore(toPrepend, inputContainer.childNodes[0]);
     return state.isFirstPrepended = false;
   } else { 
-    inputContainer.insertBefore(prepended, inputContainer.childNodes[
+    inputContainer.insertBefore(toPrepend, inputContainer.childNodes[
       childNodesLength - 2
     ]);
   }
 }
 
 function init() {
+  bindKeyEvents();
   return state = {
     inputs: [],
     isFirstPrepended: true,
-    keys: {
-      shift: false,
-      enter: false
-    }
+    clearingPastInputs: false,
   }
 }
 
