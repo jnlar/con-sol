9702663d26874cc2993890c1d31a4f99756ee971
jnlar <jon357@pm.me>
2022-02-13 16:06:07 +1100

    wrap tests in describe/s

--- src/__tests__/dom.test.js
+++ src/__tests__/dom.test.js
@@ -18,177 +18,179 @@ afterEach(() => {
   state.inputs = [];
 })
 
-describe('EventListeners', () => {
-  /*
-   * bindKeyEvents handles all of our events in the console app,
-   * triggering an event by dispatching a particular keypress will cover testing 
-   * for most of our functions binded to our keyboard events.
-   */
-  const arrowUp = new KeyboardEvent('keydown', { key: 'ArrowUp' });
-  const arrowDown = new KeyboardEvent('keydown', { key: 'ArrowDown' });
-  const enter = new KeyboardEvent('keydown', { key: 'Enter' });
-  const ctrlL = new KeyboardEvent('keydown', { key: 'l', ctrlKey: true });
+describe('DOM tests', () => {
+  describe('EventListeners', () => {
+    /*
+    * bindKeyEvents handles all of our events in the console app,
+    * triggering an event by dispatching a particular keypress will cover testing 
+    * for most of our functions binded to our keyboard events.
+    */
+    const arrowUp = new KeyboardEvent('keydown', { key: 'ArrowUp' });
+    const arrowDown = new KeyboardEvent('keydown', { key: 'ArrowDown' });
+    const enter = new KeyboardEvent('keydown', { key: 'Enter' });
+    const ctrlL = new KeyboardEvent('keydown', { key: 'l', ctrlKey: true });
 
-  beforeEach(() => {
-    document.body.innerHTML = `
-      <div id="input-container">
-        <input id="console" value="2" />
-        <div id="output">
-        </div>
-      </div>
-      `;
-    nodes.inputContainer = document.getElementById('input-container');
-    nodes.textConsole = document.getElementById('console');
-    nodes.output = document.getElementById('output');
-    state.firstTraversal = true;
-    state.inputs = [];
-    bindKeyEvents();
-  })
-
-  describe('traverseListener', () => {
-    test('on ArrowUp should set the console input to the last input executed, and set state.firstTraversal to false', () => {
-      state.inputs = ['hey', 'friend'];
-      expect(state.firstTraversal).toBe(true);
-      nodes.textConsole.dispatchEvent(arrowUp);
-      expect(nodes.textConsole.value).toBe('friend');
-      expect(state.firstTraversal).toBe(false);
-    })
-
-    test('on ArrowDown should set the console input to the previously traversed input if possible', () => {
-      state.inputs = ['hey', 'friend'];
-      expect(state.firstTraversal).toBe(true);
-      nodes.textConsole.dispatchEvent(arrowUp);
-      expect(nodes.textConsole.value).toBe('friend');
-      nodes.textConsole.dispatchEvent(arrowUp);
-      expect(nodes.textConsole.value).toBe('hey');
-      nodes.textConsole.dispatchEvent(arrowDown);
-      expect(nodes.textConsole.value).toBe('friend');
-    })
-  })
-
-  describe('executeListener', () => {
-    test('the prepended past input div should have the executed input as it\'s innerHTML', () => {
-      nodes.textConsole.dispatchEvent(enter);
-      expect(nodes.inputContainer.childNodes[0].innerHTML).toBe('2')
-      expect(output.innerHTML).toBe('2');
-      expect(nodes.textConsole.value).toBe('');
-      expect(state.inputs[state.inputs.length - 1]).toBe('2');
-    })
-    test('the output div should have the expected output if the input was valid JavaScript', () => {
-      document.getElementById('console').value = 'x = 3'
-      nodes.textConsole.dispatchEvent(enter);
-      expect(output.innerHTML).toBe('3');
-    })
-    test('pressing Enter should clear the console if the input was valid JavaScript', () => {
-      nodes.textConsole.dispatchEvent(enter);
-      expect(nodes.textConsole.value).toBe('');
-    })
-
-    test('pressing Enter with "clear" as the console input should remove all past inputs from the consoles display', () => {
-      // We need create a past input before we can clear 
-      nodes.textConsole.dispatchEvent(enter);
-      expect(document.querySelectorAll('.past-input').length).toBe(1);
-      nodes.textConsole.value = 'clear';
-      nodes.textConsole.dispatchEvent(enter);
-      expect(document.querySelectorAll('.past-input').length).toBe(0);
-    })
-  })
-
-  describe('clearlistener', () => {
-    test('pressing crtl and l in combination should clear the past inputs displayed in the console', () => {
+    beforeEach(() => {
       document.body.innerHTML = `
         <div id="input-container">
-          <div class="past-input">y = a => b => a * b</div>
-          <div class="past-input">Math.pow(2, 10)</div>
           <input id="console" value="2" />
           <div id="output">
           </div>
         </div>
-      `;
-      expect(document.querySelectorAll('.past-input').length).toBe(2);
-      nodes.textConsole.dispatchEvent(ctrlL);
-      expect(document.querySelectorAll('.past-input').length).toBe(0);
+        `;
+      nodes.inputContainer = document.getElementById('input-container');
+      nodes.textConsole = document.getElementById('console');
+      nodes.output = document.getElementById('output');
+      state.firstTraversal = true;
+      state.inputs = [];
+      bindKeyEvents();
+    })
+
+    describe('traverseListener', () => {
+      test('on ArrowUp should set the console input to the last input executed, and set state.firstTraversal to false', () => {
+        state.inputs = ['hey', 'friend'];
+        expect(state.firstTraversal).toBe(true);
+        nodes.textConsole.dispatchEvent(arrowUp);
+        expect(nodes.textConsole.value).toBe('friend');
+        expect(state.firstTraversal).toBe(false);
+      })
+
+      test('on ArrowDown should set the console input to the previously traversed input if possible', () => {
+        state.inputs = ['hey', 'friend'];
+        expect(state.firstTraversal).toBe(true);
+        nodes.textConsole.dispatchEvent(arrowUp);
+        expect(nodes.textConsole.value).toBe('friend');
+        nodes.textConsole.dispatchEvent(arrowUp);
+        expect(nodes.textConsole.value).toBe('hey');
+        nodes.textConsole.dispatchEvent(arrowDown);
+        expect(nodes.textConsole.value).toBe('friend');
+      })
+    })
+
+    describe('executeListener', () => {
+      test('the prepended past input div should have the executed input as it\'s innerHTML', () => {
+        nodes.textConsole.dispatchEvent(enter);
+        expect(nodes.inputContainer.childNodes[0].innerHTML).toBe('2')
+        expect(output.innerHTML).toBe('2');
+        expect(nodes.textConsole.value).toBe('');
+        expect(state.inputs[state.inputs.length - 1]).toBe('2');
+      })
+      test('the output div should have the expected output if the input was valid JavaScript', () => {
+        document.getElementById('console').value = 'x = 3'
+        nodes.textConsole.dispatchEvent(enter);
+        expect(output.innerHTML).toBe('3');
+      })
+      test('pressing Enter should clear the console if the input was valid JavaScript', () => {
+        nodes.textConsole.dispatchEvent(enter);
+        expect(nodes.textConsole.value).toBe('');
+      })
+
+      test('pressing Enter with "clear" as the console input should remove all past inputs from the consoles display', () => {
+        // We need create a past input before we can clear 
+        nodes.textConsole.dispatchEvent(enter);
+        expect(document.querySelectorAll('.past-input').length).toBe(1);
+        nodes.textConsole.value = 'clear';
+        nodes.textConsole.dispatchEvent(enter);
+        expect(document.querySelectorAll('.past-input').length).toBe(0);
+      })
+    })
+
+    describe('clearlistener', () => {
+      test('pressing crtl and l in combination should clear the past inputs displayed in the console', () => {
+        document.body.innerHTML = `
+          <div id="input-container">
+            <div class="past-input">y = a => b => a * b</div>
+            <div class="past-input">Math.pow(2, 10)</div>
+            <input id="console" value="2" />
+            <div id="output">
+            </div>
+          </div>
+        `;
+        expect(document.querySelectorAll('.past-input').length).toBe(2);
+        nodes.textConsole.dispatchEvent(ctrlL);
+        expect(document.querySelectorAll('.past-input').length).toBe(0);
+      })
     })
   })
-})
 
-describe('executeInput', () => {
-  // EventListeners tests didn't cover some parts of the executeInput
-  // function, so we cover the missing parts here.
-  const errorColor = 'text-red-600'
+  describe('executeInput', () => {
+    // EventListeners tests didn't cover some parts of the executeInput
+    // function, so we cover the missing parts here.
+    const errorColor = 'text-red-600'
 
-  beforeEach(() => {
-    document.body.innerHTML = `<input id="console" value="" /><div id="output" class="${errorColor}"></div>`;
-    nodes.output = document.getElementById('output');
+    beforeEach(() => {
+      document.body.innerHTML = `<input id="console" value="" /><div id="output" class="${errorColor}"></div>`;
+      nodes.output = document.getElementById('output');
+    })
+
+    test('Should throw an error if the input value is invalid JavaScript', () => {
+      expect(() => {executeInput('hi')}).toThrow();
+      expect(nodes.output.classList.contains(errorColor)).toBe(true);
+    })
+
+    test('Should remove the errorColor class from the output div if the input just executed was valid JavaScript', () => {
+      expect(nodes.output.classList.contains(errorColor)).toBe(true);
+      executeInput('x = (a, b) => a + b');
+      expect(nodes.output.classList.contains(errorColor)).toBe(false);
+    })
   })
 
-  test('Should throw an error if the input value is invalid JavaScript', () => {
-    expect(() => {executeInput('hi')}).toThrow();
-    expect(nodes.output.classList.contains(errorColor)).toBe(true);
+  describe('createNewChild', () => {
+    test('Should return a HTMLDivElement', () => {
+      expect(createNewChild().toString()).toBe("[object HTMLDivElement]");
+    })
   })
 
-  test('Should remove the errorColor class from the output div if the input just executed was valid JavaScript', () => {
-    expect(nodes.output.classList.contains(errorColor)).toBe(true);
-    executeInput('x = (a, b) => a + b');
-    expect(nodes.output.classList.contains(errorColor)).toBe(false);
-  })
-})
+  describe('removeNodes', () => {
+    test('Should remove all found elements', () => {
+      document.body.innerHTML = '<div class="past-input"></div><div class="past-input"></div>'
+      const result = removeNodes('.past-input');
 
-describe('createNewChild', () => {
-  test('Should return a HTMLDivElement', () => {
-    expect(createNewChild().toString()).toBe("[object HTMLDivElement]");
-  })
-})
+      expect(result).toBe(true);
+      expect(document.querySelectorAll('.past-input').length).toBe(0);
+    })
 
-describe('removeNodes', () => {
-  test('Should remove all found elements', () => {
-    document.body.innerHTML = '<div class="past-input"></div><div class="past-input"></div>'
-    const result = removeNodes('.past-input');
-
-    expect(result).toBe(true);
-    expect(document.querySelectorAll('.past-input').length).toBe(0);
+    test('Should throw an error if no elements are found', () => {
+      expect(() => removeNodes('foo')).toThrow();
+    })
   })
 
-  test('Should throw an error if no elements are found', () => {
-    expect(() => removeNodes('foo')).toThrow();
-  })
-})
+  describe('setInpuToHistory', () => {
+    test('Should set the console input to the given index in the state.inputs array', () => {
+      document.body.innerHTML = '<input id="console" value="foo"/>';
+      nodes.textConsole = document.getElementById('console')
+      state.inputs = ['hello', 'world'];
 
-describe('setInpuToHistory', () => {
-  test('Should set the console input to the given index in the state.inputs array', () => {
-    document.body.innerHTML = '<input id="console" value="foo"/>';
-    nodes.textConsole = document.getElementById('console')
-    state.inputs = ['hello', 'world'];
-
-    expect(setInputToHistory(1)).toBe('world');
-  })
-})
-
-describe('prependPastInput', () => {
-  test('Should set isFirstPrepended to false if the past input is the first past input to be inserted into input-container', () => {
-    document.body.innerHTML = '<div id="input-container"></div>';
-    nodes.inputContainer = document.getElementById('input-container');
-    state.inputs = ['bar'];
-    state.isFirstPrepended = true;
-
-    expect(prependPastInput()).toBe(false);
-    expect(nodes.inputContainer.childNodes[0].innerHTML).toBe('bar');
+      expect(setInputToHistory(1)).toBe('world');
+    })
   })
 
-  test('Should prepend the past input above the console input, and below every other past input so long as their are > 1 past inputs', () => {
-    document.body.innerHTML = 
-    `<div id="input-container">
-      <div class="past-input">hey</div>
-      <div class="past-input">hi</div>
-      <input id="console" value="foo" />
-    </div>`;
-    nodes.inputContainer = document.getElementById('input-container');
-    state.isFirstPrepended = false;
-    state.inputs = ['yo'];
+  describe('prependPastInput', () => {
+    test('Should set isFirstPrepended to false if the past input is the first past input to be inserted into input-container', () => {
+      document.body.innerHTML = '<div id="input-container"></div>';
+      nodes.inputContainer = document.getElementById('input-container');
+      state.inputs = ['bar'];
+      state.isFirstPrepended = true;
 
-    prependPastInput();
-    expect(nodes.inputContainer.childNodes[
-      nodes.inputContainer.childNodes.length - 3
-    ].innerHTML).toBe('yo');
+      expect(prependPastInput()).toBe(false);
+      expect(nodes.inputContainer.childNodes[0].innerHTML).toBe('bar');
+    })
+
+    test('Should prepend the past input above the console input, and below every other past input so long as their are > 1 past inputs', () => {
+      document.body.innerHTML = 
+      `<div id="input-container">
+        <div class="past-input">hey</div>
+        <div class="past-input">hi</div>
+        <input id="console" value="foo" />
+      </div>`;
+      nodes.inputContainer = document.getElementById('input-container');
+      state.isFirstPrepended = false;
+      state.inputs = ['yo'];
+
+      prependPastInput();
+      expect(nodes.inputContainer.childNodes[
+        nodes.inputContainer.childNodes.length - 3
+      ].innerHTML).toBe('yo');
+    })
   })
 })
--- src/__tests__/main.test.js
+++ src/__tests__/main.test.js
@@ -18,97 +18,99 @@ afterEach(() => {
   traverse.position = 1;
 })
 
-describe('pushInput', () => {
-  test('Should push one element onto an array', () => {
-    const array = [];
-    jest.fn(() => {
-      return pushInput(array, 'foo');
-    })()
-    expect(array).toEqual(['foo']);
+describe('Main', () => {
+  describe('pushInput', () => {
+    test('Should push one element onto an array', () => {
+      const array = [];
+      jest.fn(() => {
+        return pushInput(array, 'foo');
+      })()
+      expect(array).toEqual(['foo']);
+    })
   })
-})
 
-describe('handleClear', () => {
-  test('Should set state.clearingPastInputs to false', () => {
-    const state = {clearingPastInputs: true}
-    jest.fn(() => {
-      return handleClear(state);
-    })()
-    expect(state.clearingPastInputs).toBe(false);
+  describe('handleClear', () => {
+    test('Should set state.clearingPastInputs to false', () => {
+      const state = {clearingPastInputs: true}
+      jest.fn(() => {
+        return handleClear(state);
+      })()
+      expect(state.clearingPastInputs).toBe(false);
+    })
   })
-})
 
-describe('clearInputs', () => {
-  test('Should return an empty array', () => {
-    expect(clearInputs()).toEqual([]);
+  describe('clearInputs', () => {
+    test('Should return an empty array', () => {
+      expect(clearInputs()).toEqual([]);
+    })
   })
-})
 
-describe('doCallback', () => {
-  test('Should execute a callback function', () => {
-    expect(doCallback(() => 1 + 2)).toBe(3);
+  describe('doCallback', () => {
+    test('Should execute a callback function', () => {
+      expect(doCallback(() => 1 + 2)).toBe(3);
+    })
+    test('Should return null if the first arguement isn\'t a function', () => {
+      expect(doCallback(3)).toBe(null);
+      expect(doCallback('hey')).toBe(null);
+    })
   })
-  test('Should return null if the first arguement isn\'t a function', () => {
-    expect(doCallback(3)).toBe(null);
-    expect(doCallback('hey')).toBe(null);
-  })
-})
 
-describe('Commands', () => {
-  test('traverse.executeCommand with AddCommand instantiation as parameter should add to traverse.position', () => {
-    const traverse = new Traverse();
-    traverse.executeCommand(new AddCommand(2));
-    expect(traverse.position).toBe(3);
+  describe('Commands', () => {
+    test('traverse.executeCommand with AddCommand instantiation as parameter should add to traverse.position', () => {
+      const traverse = new Traverse();
+      traverse.executeCommand(new AddCommand(2));
+      expect(traverse.position).toBe(3);
+    })
+    test('traverse.undo should undo the last command', () => {
+      const traverse = new Traverse();
+      traverse.executeCommand(new AddCommand(2));
+      traverse.undo();
+      expect(traverse.position).toBe(1);
+    })
   })
-  test('traverse.undo should undo the last command', () => {
-    const traverse = new Traverse();
-    traverse.executeCommand(new AddCommand(2));
-    traverse.undo();
-    expect(traverse.position).toBe(1);
-  })
-})
 
-describe('canTraverseBack', () => {
-  afterEach(() => {
-    state.inputs = [];
+  describe('canTraverseBack', () => {
+    afterEach(() => {
+      state.inputs = [];
+    })
+    test('Should return a callback', () => {
+      state.inputs = ['foo', 'bar'];
+      const mockFn = jest.fn(() => {
+        return 'foo';
+      });
+      canTraverseBack(mockFn)
+      expect(mockFn).toBeCalled();
+    })
+    test('Should throw an error if inputs array is empty', () => {
+      expect(() => {
+        canTraverseBack(() => 'foo');
+      }).toThrow();
+    })
+    test('Should throw an error if traverse.position is equal to state.inputs.length', () => {
+      state.inputs = ['foo', 'bar'];
+      traverse.position = 2;
+      expect(() => {
+        canTraverseBack(() => 'foo');
+      }).toThrow();
+    })
   })
-  test('Should return a callback', () => {
-    state.inputs = ['foo', 'bar'];
-    const mockFn = jest.fn(() => {
-      return 'foo';
-    });
-    canTraverseBack(mockFn)
-    expect(mockFn).toBeCalled();
-  })
-  test('Should throw an error if inputs array is empty', () => {
-    expect(() => {
-      canTraverseBack(() => 'foo');
-    }).toThrow();
-  })
-  test('Should throw an error if traverse.position is equal to state.inputs.length', () => {
-    state.inputs = ['foo', 'bar'];
-    traverse.position = 2;
-    expect(() => {
-      canTraverseBack(() => 'foo');
-    }).toThrow();
-  })
-})
 
-describe('canTraverseForward', () => {
-  test('Should throw an error if traverse position is 1', () => {
-    const mockFn = jest.fn(() => {
-      return 'foo';
-    });
-    expect(() => {
+  describe('canTraverseForward', () => {
+    test('Should throw an error if traverse position is 1', () => {
+      const mockFn = jest.fn(() => {
+        return 'foo';
+      });
+      expect(() => {
+        canTraverseForward(mockFn);
+      }).toThrow();
+    })
+    test('Should return a callback if traverse position isn\'t 1', () => {
+      traverse.position = 2;
+      const mockFn = jest.fn(() => {
+        return 'foo';
+      });
       canTraverseForward(mockFn);
-    }).toThrow();
-  })
-  test('Should return a callback if traverse position isn\'t 1', () => {
-    traverse.position = 2;
-    const mockFn = jest.fn(() => {
-      return 'foo';
-    });
-    canTraverseForward(mockFn);
-    expect(mockFn).toBeCalled();
+      expect(mockFn).toBeCalled();
+    })
   })
 })
